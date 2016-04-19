aebe1fecd7e3738720b89f45313606c226ec40cc
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-19 19:17:05 +0200

    renamed sql

---
+++ src/main/resources/sql/v1__import.sql
@@ -0,0 +1,17 @@
+/* Single line comment */
+CREATE TABLE test_user (
+  name VARCHAR(25) NOT NULL,
+  PRIMARY KEY(name)
+);
+
+/*
+Multi-line
+comment
+*/
+
+-- Placeholder
+INSERT INTO test_user (name) VALUES ('Mr. T');
+
+
+INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-1, 'jdoe', 'John', 'Doe');
+INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-2, 'emuster', 'Erika', 'Mustermann'); 
