bdcdd93e17b0295b687e013a08594e16d93a090e
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-19 21:41:48 +0200

    updated sql

--- src/main/resources/sql/v1__import.sql
+++ src/main/resources/sql/v1__import.sql
@@ -1,17 +1,18 @@
-/* Single line comment */
 CREATE TABLE test_user (
   name VARCHAR(25) NOT NULL,
   PRIMARY KEY(name)
 );
 
-/*
-Multi-line
-comment
-*/
-
--- Placeholder
 INSERT INTO test_user (name) VALUES ('Mr. T');
 
+
+CREATE TABLE USERS (
+  ID        INT          NOT NULL,
+  USERNAME  VARCHAR(255) NOT NULL,
+  FIRSTNAME VARCHAR(255) NOT NULL,
+  LASTNAME  VARCHAR(255) NOT NULL,
+  PRIMARY KEY(ID)
+);
 
 INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-1, 'jdoe', 'John', 'Doe');
 INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-2, 'emuster', 'Erika', 'Mustermann'); 
