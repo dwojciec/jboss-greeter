3035e2262210b1ad419dbb91bdce7f0ee58db9a8
Samuel Terburg <Samuel.Terburg@Panther-IT.nl>
2016-04-20 13:37:12 +0200

    added v2 sql

---
+++ src/main/resources/sql/v2__moreusers.sql
@@ -0,0 +1,2 @@
+INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-3, 'sterburg', 'Samuel', 'Terburg');
+INSERT INTO USERS (ID, USERNAME, FIRSTNAME, LASTNAME) VALUES (-4, 'sscheele', 'Sebastian', 'Scheele'); 
